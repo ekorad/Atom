bed491c4ac8d11ad34f3ae20022492c55ed835da
Vlad Zahiu <vladzahiu28@gmail.com>
2020-12-27 15:52:38 +0200

    Added database table initialization from file
    Configured initialization in application properties
    
    Changed UserPermission entity id generation type
    
    Added data.sql for table initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user_permissions (`name`) VALUES
+    ('READ'), ('WRITE'), ('MODIFY'), ('DELETE'),
+    ('ELEVATED_READ'), ('ELEVATED_WRITE'), ('ELEVATED_MODIFY'), ('ELEVATED_DELETE'),
+    ('DEV_READ'), ('DEV_WRITE'), ('DEV_MODIFY'), ('DEV_DELETE');

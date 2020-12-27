14462e25db0678a504ef486f1d32ef961acdc8e3
Vlad Zahiu <vladzahiu28@gmail.com>
2020-12-27 16:00:52 +0200

    Removed/Commented out unnecessary code
    User permissions controller should only alow a 'get all' operation
    
    Added a 'delete all' statement to the sql file as it gets execute each time the context is refreshed (to avoid duplication exceptions)

--- src/main/java/com/atom/application/controllers/UserPermissionController.java
+++ src/main/java/com/atom/application/controllers/UserPermissionController.java
@@ -2,22 +2,14 @@ package com.atom.application.controllers;
 
 import java.util.List;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 import com.atom.application.models.UserPermission;
 import com.atom.application.services.UserPermissionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -29,21 +21,23 @@ public class UserPermissionController {
     @Autowired
     private UserPermissionService service;
 
-    @PostMapping(path = "/add", params = "permissionName")
+    // TODO: remove all comented code when possible
+
+    /*@PostMapping(path = "/add", params = "permissionName")
     @ResponseStatus(HttpStatus.CREATED)
     public UserPermission addNewPermission(@RequestParam @NotBlank @Size(min = 3, max = 50) String permissionName) {
         UserPermission perm = new UserPermission();
         perm.setName(permissionName);
         return service.addPermission(perm);
-    }
+    }*/
 
-    @PutMapping(path = "/update", params = { "oldPermissionName", "newPermissionName" })
+    /*@PutMapping(path = "/update", params = { "oldPermissionName", "newPermissionName" })
     public UserPermission updatePermission(@RequestParam @NotBlank @Size(min = 3, max = 50) String oldPermissionName,
             @RequestParam @NotBlank @Size(min = 3, max = 50) String newPermissionName) {
         UserPermission perm = service.getPermissionByName(oldPermissionName);
         perm.setName(newPermissionName);
-        return service.updatePermission(perm);
-    }
+        retur*n service.updatePermission(perm);
+    }*/
 
     @GetMapping
     public List<UserPermission> getAllPermissions() {
@@ -54,21 +48,21 @@ public class UserPermissionController {
         return permissions;
     }
 
-    @GetMapping(params = "permissionName")
+    /*@GetMapping(params = "permissionName")
     public UserPermission getPermissionByName(@RequestParam @NotBlank @Size(min = 3, max = 50) String permissionName) {
         UserPermission perm = service.getPermissionByName(permissionName);
         if (perm == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         return perm;
-    }
+    }*/
 
-    @DeleteMapping(path = "/delete", params = "permissionName")
+    /*@DeleteMapping(path = "/delete", params = "permissionName")
     public void deletePermissionByName(@RequestParam @NotBlank @Size(min = 3, max = 50) String permissionName) {
         UserPermission perm = service.getPermissionByName(permissionName);
         if (perm == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         service.removePermission(perm);
-    }
+    }*/
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
+DELETE FROM user_permissions;
+
 INSERT INTO user_permissions (`name`) VALUES
     ('READ'), ('WRITE'), ('MODIFY'), ('DELETE'),
     ('ELEVATED_READ'), ('ELEVATED_WRITE'), ('ELEVATED_MODIFY'), ('ELEVATED_DELETE'),

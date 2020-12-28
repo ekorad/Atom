d05e9fb91e4d31387d30cf1c4e06484d69532168
Vlad Zahiu <vladzahiu28@gmail.com>
2020-12-28 14:55:33 +0200

    Added "no permission" case to permission view page
    Fixed code so that it works with *ngIf

--- client/src/app/components/user-permission-admin/user-permission-admin.component.ts
+++ client/src/app/components/user-permission-admin/user-permission-admin.component.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort, MatSortable } from '@angular/material/sort';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { UserPermission } from './../../models/user-permission';
 import { UserPermissionService } from './../../services/user-permission.service';
@@ -10,24 +10,29 @@ import { UserPermissionService } from './../../services/user-permission.service'
   templateUrl: './user-permission-admin.component.html',
   styleUrls: ['./user-permission-admin.component.css']
 })
-export class UserPermissionAdminComponent implements AfterViewInit {
+export class UserPermissionAdminComponent implements OnInit {
 
   dataSource: MatTableDataSource<UserPermission> = new MatTableDataSource<UserPermission>();
   columnsToDisplay: string[] = ['id', 'name'];
 
-  @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false }) set sort(val: MatSort) {
+    if (val) {
+      this.dataSource.sort = val;
+    }
+  }
+
+  @ViewChild(MatPaginator, { static: false }) set paginator(val: MatPaginator) {
+    if (val) {
+      this.dataSource.paginator = val;
+    }
+  }
 
   constructor(private userPermService: UserPermissionService) {
   }
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     this.userPermService.getAllUserPermissions()
-      .subscribe(recvPerms => {
-        this.dataSource.data = recvPerms;
-        this.dataSource.sort = this.sort;
-        this.dataSource.paginator = this.paginator;
-      });
+      .subscribe(recvPerms => this.dataSource.data = recvPerms);
   }
 
   applyFilter(event: Event): void {
